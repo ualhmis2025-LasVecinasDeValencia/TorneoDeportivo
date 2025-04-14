6ca45e53a396ac116be955309e9d8c05d0595724
Antonio Gallardo <agg860@inlumine.ual.es>
2025-04-14 20:28:35 +0200

    Aumentada la cobertura en distintas clases y nuevo método.
    Se ha añadido un nuevo método para cumplir con el requisito: "✅ Un
    equipo no puede estar registrado en más de un torneo simultáneamente."

--- src/main/java/org/ualhmis/torneos/GestorTorneos.java
+++ src/main/java/org/ualhmis/torneos/GestorTorneos.java
@@ -10,7 +10,41 @@ class GestorTorneos {
         this.torneos = new ArrayList<>();
     }
 
-    public void crearTorneo(String nombre, String deporte, String categoria, String modalidad, String tipo) {
-        torneos.add(new Torneo(nombre, deporte, categoria, modalidad, tipo));
+    public Torneo crearTorneo(String nombre, String deporte, String categoria, String modalidad, String tipo) {
+    	
+    	Torneo Nuevotorneo = new Torneo(nombre, deporte, categoria, modalidad, tipo); 
+        torneos.add(Nuevotorneo);
+        
+        return Nuevotorneo; 
     }
+    
+    public boolean RegistrarEquipoTorneo(Equipo equipo, Torneo torneo) {
+    	
+    	
+    	for(Torneo torneo1 : this.torneos) {
+    		
+    		for(Equipo equipos : torneo1.getEquipos()) {
+    			
+    			if(equipos.equals(equipo)) {
+    				return false; 
+    			}
+	
+    		}
+    	}
+    	
+    	torneo.registrarEquipo(equipo);
+    	return true; 
+    	
+    
+    }
+    
+    
+    
+    public List<Torneo> getTorneos(){
+    	
+    	return torneos; 
+    }
+    
+    
+    
 }
