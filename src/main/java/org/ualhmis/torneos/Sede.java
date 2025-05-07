98eaa42ef2bd6bb638c426381b009373cc02d6ab
Miguel Angel Martinez Simon <mms172@inlumine.ual.es>
2025-05-07 13:01:48 +0200

    Push para probar Jenkins

--- src/main/java/org/ualhmis/torneos/Sede.java
+++ src/main/java/org/ualhmis/torneos/Sede.java
@@ -19,12 +19,7 @@ public class Sede {
    }
    
 
-//public Sede(String nombre) {
-//	  
-//	  this.nombre = nombre; 
-//	  this.instalaciones = new ArrayList<Instalacion>();
-//	  
-//  }
+
   
    
    public boolean AñadirInstalacion(Instalacion instalacion) { //Si cambiamos instalaciones por un HashSet ya no acepta duplicados, pero si se hace antes hay que añadir metodo equals y metodo hashcode(o como se llame) a Instalacion
